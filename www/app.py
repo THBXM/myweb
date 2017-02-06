5678534119f578f99ece7dbcf0718939a9a6fe7e
chuyang1234567 <2471099610@qq.com>
2017-02-06 15:29:27 +0800

    day2

---
+++ www/app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+#-*-coding:utf-8-*-
+
+__author__='Jon Lai'
+
+'''
+async web application.
+'''
+
+import logging;logging.basicConfig(level=logging.INFO)
+
+import asyncio,os,json,time
+from datetime import datetime
+
+from aiohttp import web
+def index(request):
+    return web.Response(body=b"<h1>Yes,I can</h1>",content_type="text/html",charset="utf-8")
+async def init(loop):
+    app=web.Application(loop=loop)
+    app.router.add_route("GET","/",index)
+    if __name__=='__main__':
+        srv=await loop.create_server(app.make_handler(),"127.0.0.1",9000)
+        logging.info("service started at http://127.0.0.1:9000...")
+        return srv
+loop=asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
